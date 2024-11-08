53dc10b9ebdaf73e2335af5955403680e96c130c
= <=>
2024-11-08 16:43:57 +0800

    menambahkan file about.php didalam folder controllers

--- app/core/App.php
+++ app/core/App.php
@@ -17,7 +17,20 @@ class App {
         }
 
         require_once '../App/controllers/' . $this->controller . '.php';
-        $this->controller = new $this->controller;;
+        $this->controller = new $this->controller;
+
+        // method
+        if( isset($url[1]) ) {
+            if( method_exists($this->controller, $url[1]) ) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // Kelola parameternya
+        if( !empty($url) ) {
+            var_dump($url);
+        }
     }
 
     public function parseURL() 
