927374f7923ab750171be93924f311e4e3d94caa
= <=>
2024-11-09 06:27:01 +0800

    menambahkan parameter data didalam header agar bisa berganti ganti namanya

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -3,14 +3,20 @@
 class About extends Controller {
     public function index($nama = 'urel', $umur = 0, $pekerjaan = 'mahasiswa')
     {
+        $data["judul"] = 'About Me';
         $data['nama'] = $nama;
         $data['umur'] = $umur;
         $data['pekerjaan'] = $pekerjaan;
+        $this->view('templates/header', $data);
         $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     
     public function page()
     {
+        $data["judul"] = 'Pages';
+        $this->view('templates/header', $data);
         $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
-</head>
-<body>
-    <h1>About Me</h1>
-    <p>Hallo nama saya adalah <?= $data['nama']; ?>, saya berumur <?= $data['umur']; ?> tahun dan saya adalah seorang <?= $data['pekerjaan']; ?></p>
-</body>
-</html>
+<h1>About Me</h1>
+<p>Hallo nama saya adalah <?= $data['nama']; ?>, saya berumur <?= $data['umur']; ?> tahun dan saya adalah seorang <?= $data['pekerjaan']; ?></p>
