5d469cc90d1a36e0b665219060024001f52c3715
efraim urel <efraimurelpalodang@gmail.com>
2024-11-10 05:42:53 +0800

    menambahkan image pada halaman about

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,5 @@
 <div class="container mt-3">
   <h1>About Me</h1>
+  <img src="<?= BASEURL; ?>/img/image.png" class="img-thumbnail rounded-circle shadow mb-3" alt="my image" width="200">
   <p>Hallo nama saya adalah <?= $data['nama']; ?>, saya berumur <?= $data['umur']; ?> tahun dan saya adalah seorang <?= $data['pekerjaan']; ?></p>
 </div>
