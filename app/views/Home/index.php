e6beb4dd92776596ca82e533a2529610c0f705ab
efraim urel <efraimurelpalodang@gmail.com>
2024-11-10 05:31:15 +0800

    membuat style jumbotron pada halaman home

--- app/views/Home/index.php
+++ app/views/Home/index.php
@@ -1,1 +1,9 @@
-<h1>Selamat Datang Diwebsite saya</h1>
+<div class="container">
+  <section class="hero bg-secondary text-white py-5 mt-3">
+    <div class="container text-center">
+      <h1 class="display-4">Selamat Datang di Website Kami!</h1>
+      <p class="lead">Kami menyediakan solusi terbaik untuk kebutuhan Anda.</p>
+      <a href="#" class="btn btn-light btn-lg">Pelajari Lebih Lanjut</a>
+    </div>
+  </section>
+</div>
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,4 @@
-<h1>About Me</h1>
-<p>Hallo nama saya adalah <?= $data['nama']; ?>, saya berumur <?= $data['umur']; ?> tahun dan saya adalah seorang <?= $data['pekerjaan']; ?></p>
+<div class="container mt-3">
+  <h1>About Me</h1>
+  <p>Hallo nama saya adalah <?= $data['nama']; ?>, saya berumur <?= $data['umur']; ?> tahun dan saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
