572282b9d6884d5a8a71665aea18ed87ba835ea6
efraim urel <efraimurelpalodang@gmail.com>
2024-11-11 06:17:42 +0800

    membuat class mahasiswa_model dan membuat method getAllMahasiswa untuk mengambil semua data mahasiswa, lalu memanggilnya didalam folder Controllers didalam file Mahasiswa.php

---
+++ app/models/Mahasiswa_model.php
@@ -0,0 +1,29 @@
+<?php
+
+class Mahasiswa_model {
+  private $mhs = [
+    [
+      "nama" => "Efraim urel palodang",
+      "npm" => "1231278492",
+      "email" => "kepooya@gmail.com",
+      "jurusan" => "Teknik Informatika"
+    ],
+    [
+      "nama" => "Molly aja lah",
+      "npm" => "4748292749",
+      "email" => "mollynigeng@gmail.com",
+      "jurusan" => "Perikanan"
+    ],
+    [
+      "nama" => "Bong ni boss",
+      "npm" => "8593028949",
+      "email" => "bongnisenggol@gmail.com",
+      "jurusan" => "Perkebunan"
+    ]
+  ]
+
+  public function getAllMahasiswa()
+  {
+    return $this->mhs;
+  }
+}
