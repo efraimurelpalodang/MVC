2236154b738680241c9a9505e2605a025399ecca
efraim urel <efraimurelpalodang@gmail.com>
2024-11-11 05:38:44 +0800

    memperbaiki jumbotron yang ada didalam halaman home

--- app/views/Home/index.php
+++ app/views/Home/index.php
@@ -1,7 +1,8 @@
 <div class="container">
   <section class="hero bg-secondary text-white py-5 mt-3">
-    <div class="container text-center">
-      <h1 class="display-4">Selamat Datang di Website Kami!</h1>
+    <div class="container text-start ms-4">
+      <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+      <p class="fs-4 fw-light pb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ad in officia, delectus dicta sint eaque totam porro exercitationem laboriosam!</p>
       <p class="lead">Kami menyediakan solusi terbaik untuk kebutuhan Anda.</p>
       <a href="#" class="btn btn-light btn-lg">Pelajari Lebih Lanjut</a>
     </div>
