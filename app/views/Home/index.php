67b770bb9211e5932aa7950397ee86479595b1d5
efraim urel <efraimurelpalodang@gmail.com>
2024-11-11 05:57:24 +0800

    membuat method model didalam class controller untuk menjalankan modelnya

--- app/views/Home/index.php
+++ app/views/Home/index.php
@@ -2,7 +2,7 @@
   <section class="hero bg-secondary text-white py-5 mt-3">
     <div class="container text-start ms-4">
       <h1 class="display-4">Selamat Datang di Website Saya!</h1>
-      <p class="fs-4 fw-light pb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ad in officia, delectus dicta sint eaque totam porro exercitationem laboriosam!</p><hr>
+      <p class="fs-4 fw-light pb-3">Hallo, nama saya <?= $data["nama"]; ?></p><hr>
       <p class="lead">Kami menyediakan solusi terbaik untuk kebutuhan Anda.</p>
       <a href="#" class="btn btn-light btn-lg">Pelajari Lebih Lanjut</a>
     </div>
