da63912d5efa491cf2888ed368f7538bef028fae
efraim urel <efraimurelpalodang@gmail.com>
2024-11-13 14:20:15 +0800

    membuat list group

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -3,14 +3,12 @@
     <div class="col-6">
       <h3>Daftar Mahasiswa</h3>
 
-      <?php foreach( $data["mhs"] as $mhs ) : ?>
-        <ul>
-          <li><?= $mhs['nama']; ?></li>
-          <li><?= $mhs['npm']; ?></li>
-          <li><?= $mhs['email']; ?></li>
-          <li><?= $mhs['jurusan']; ?></li>
+        <ul class="list-group">
+          <?php foreach( $data["mhs"] as $mhs ) : ?>
+          <li class="list-group-item"><?= $mhs["nama"]; ?></li>
+          <?php endforeach; ?>
         </ul>
-      <?php endforeach; ?>
+
     </div>
   </div>
 </div>
