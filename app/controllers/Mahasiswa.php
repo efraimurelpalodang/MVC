e6777645495bff514e7c2be3dd18cecd80144f09
efraim urel <efraimurelpalodang@gmail.com>
2024-11-14 05:46:58 +0800

    membuat file detail untuk menampilkan detail data mahasiswa

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -15,7 +15,7 @@ class Mahasiswa extends Controller {
     $data["judul"] = 'Detail Mahasiswa';
     $data["mhs"] = $this->model('Mahasiswa_model')->getMahasiswaById($id);
     $this->view('templates/header', $data);
-    $this->view('mahasiswa/index', $data);
+    $this->view('mahasiswa/detail', $data);
     $this->view('templates/footer');
   }
 }
