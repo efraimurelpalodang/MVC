5c355de3d1798e2d514386b79bb08eda60d3c1b6
efraim urel <efraimurelpalodang@gmail.com>
2024-11-16 07:37:55 +0800

    membuat method static untuk menampilkan pesan didalam file flasher

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -2,6 +2,24 @@
 
 class Flasher {
   
-  public static function setFlash($pesan, $aksi);
+  public static function setFlash($pesan, $aksi, $tipe)
+  {
+    $_SESSION['flash'] = [
+      'pesan' => $pesan,
+      'aksi' => $aksi,
+      'tipe' => $tipe
+    ];
+  }
+
+  public static function flash()
+  {
+    if( isset($_SESSION['flash']) ) {
+      echo '<div class="alert alert-'. $_SESSION['flash']['tipe'] .' alert-dismissible fade show" role="alert">
+              Data Mahasiswa '. $_SESSION['flash']['pesan'] .'<strong>'. $_SESSION['flash']['pesan'] .'</strong>
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+      unset($_SESSION['flash']);
+    }
+  }
 
 }
